98efb5ca2d60619a7635567f440504076862bf86
SubhajitGhosh5 <98100703+SubhajitGhosh5@users.noreply.github.com>
2022-01-23 20:15:20 +0530

    Create 02.printing_integer_numbers.c

---
+++ 02.printing_integer_numbers.c
@@ -0,0 +1,10 @@
+//printing integer numbers entered by the user
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("Enter a number :");
+    scanf("%d",&i);
+    printf("The integer numbers is :%d",i);
+    return 0;
+}
